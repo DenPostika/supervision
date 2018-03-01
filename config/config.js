56211bad793016e0843d4b4d283bd9c7de3cd7c7
Denis Postika <den.postika@gmail.com>
2018-03-01 12:35:42 +0200

    Fix connection timeout

--- config/config.js
+++ config/config.js
@@ -26,19 +26,18 @@ const envVarsSchema = Joi.object({
   .required();
 
 const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
-console.log(envVars);
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
 const config = {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT,
-  mongooseDebug: process.env.MONGOOSE_DEBUG,
-  jwtSecret: process.env.JWT_SECRET,
-  slackToken: process.env.SLACK_TOKEN,
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  mongooseDebug: envVars.MONGOOSE_DEBUG,
+  jwtSecret: envVars.JWT_SECRET,
+  slackToken: envVars.SLACK_TOKEN,
   mongo: {
-    host: process.env.MONGO_HOST
+    host: envVars.MONGO_HOST
   }
 };
 
--- index.js
+++ index.js
@@ -17,9 +17,18 @@ mongoose.Promise = Promise;
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`);
+mongoose.connect(mongoUri, {
+  server: {
+    auto_reconnect: true,
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 300000,
+      socketTimeoutMS: 300000,
+    },
+  },
+});
+mongoose.connection.on('error', error => {
+  throw new Error(`unable to connect to database: ${error}`);
 });
 
 // print mongoose logs in dev env
